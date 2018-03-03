7426ddc2787c18abfb04cf83b0e37a15dec79832
ivanfcomesen <ivanfcomesen@gmail.com>
2018-03-03 02:49:54 -0600

    First

---
+++ src/test/java/com/grupo/prisma/PrismaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.grupo.prisma;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PrismaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
