cdc509ea057083438e9b77bb598e99dc8fb320b5
ivanfcomesen <ivanfcomesen@gmail.com>
2018-03-04 20:17:56 -0600

    Boot, mongoDB, Maven, linux, syncronized, Versions

--- src/main/java/com/grupo/prisma/PrismaApplication.java
+++ src/main/java/com/grupo/prisma/PrismaApplication.java
@@ -3,12 +3,38 @@ package com.grupo.prisma;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoDatabase;
+
 @SpringBootApplication
 public class PrismaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PrismaApplication.class, args);
 		
-		//testing github
+		//i5FoEjFrZ9TB6TdZ
+		
+	
+		
+		/*
+		 
+		 Java Driver Example¶
+		MongoDB Version 3.6 or later
+		 MongoClientURI uri = new MongoClientURI(
+   		"mongodb+srv://ivanfcomesen:<PASSWORD>@geminis-gsyv1.mongodb.net/dbprisma");		
+		
+		MongoClient mongoClient = new MongoClient(uri);
+		MongoDatabase database = mongoClient.getDatabase("prisma");
+		
+		
+		  
+		  */
+		
+
+		  
+		
+		
+		
 	}
 }
