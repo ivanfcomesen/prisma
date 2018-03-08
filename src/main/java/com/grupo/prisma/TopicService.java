1eda65ceb4491216bbb224c3f01634bdf6577b08
ivanfcomesen <ivanfcomesen@gmail.com>
2018-03-08 01:29:39 -0600

    Update, Delete

--- src/main/java/com/grupo/prisma/TopicService.java
+++ src/main/java/com/grupo/prisma/TopicService.java
@@ -9,11 +9,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 
-	public List<Topic> topics = new ArrayList <>(Arrays.asList(
+	public List<Topic> topics = new ArrayList<>(Arrays.asList(
 			new Topic("spring", "Spring Framework", "Spring Framework Description"),
-			new Topic("java", "Java Framework", "Java Framework Description"),
-			new Topic("jascript", "JS", "Jsdes")
-			));
+			new Topic("java", "Java Framework", "Java Framework Description"), new Topic("jascript", "JS", "Jsdes")));
 
 	public List<Topic> getAllTopics() {
 		return topics;
@@ -22,9 +20,25 @@ public class TopicService {
 	public Topic getTopic(String id) {
 		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
 	}
+
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+	}
+
+	public void updateTopic(String id, Topic topic) {
+
+		for (int i = 0; i < topics.size(); i++) {
+			Topic t = topics.get(i);
+			if (t.getId().equals(id)) {				
+				topics.set(i, topic);						
+				return;
+			}
+		}
+	}
 	
-	public void  addTopic(Topic topic) {
-		 topics.add(topic);
+	public void deleteTopic(String id) {
+		
+		topics.removeIf(t -> t.getId().equals(id));
 	}
 
 }
