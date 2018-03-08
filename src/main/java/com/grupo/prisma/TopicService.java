9fcec5fd25e5e5badc4b45b35f3066ad7b2f2ba2
ivanfcomesen <ivanfcomesen@gmail.com>
2018-03-08 00:47:12 -0600

    Mutable

--- src/main/java/com/grupo/prisma/TopicService.java
+++ src/main/java/com/grupo/prisma/TopicService.java
@@ -1,5 +1,6 @@
 package com.grupo.prisma;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,8 +9,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 
-	public List<Topic> topics = Arrays.asList(new Topic("spring", "Spring Framework", "Spring Framework Description"),
-			new Topic("java", "Java Framework", "Java Framework Description"), new Topic("jascript", "JS", "Jsdes"));
+	public List<Topic> topics = new ArrayList <>(Arrays.asList(
+			new Topic("spring", "Spring Framework", "Spring Framework Description"),
+			new Topic("java", "Java Framework", "Java Framework Description"),
+			new Topic("jascript", "JS", "Jsdes")
+			));
 
 	public List<Topic> getAllTopics() {
 		return topics;
