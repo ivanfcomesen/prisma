c2b19cf1c639870add58201e0cee887676ce2ecc
ivanfcomesen <ivanfcomesen@gmail.com>
2018-03-10 17:29:54 -0600

    Basic Crud

--- src/main/java/com/grupo/prisma/TopicService.java
+++ src/main/java/com/grupo/prisma/TopicService.java
@@ -30,7 +30,7 @@ public class TopicService {
 		for (int i = 0; i < topics.size(); i++) {
 			Topic t = topics.get(i);
 			if (t.getId().equals(id)) {				
-				topics.set(i, topic);						
+				topics.set(i,topic);						
 				return;
 			}
 		}
