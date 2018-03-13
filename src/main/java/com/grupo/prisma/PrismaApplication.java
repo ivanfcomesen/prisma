4b445a2f02f19ac82b6a53f0ba2f401cd51dbbdf
ivanfcomesen <ivanfcomesen@gmail.com>
2018-03-13 02:39:27 -0600

    All clear

--- src/main/java/com/grupo/prisma/PrismaApplication.java
+++ src/main/java/com/grupo/prisma/PrismaApplication.java
@@ -1,13 +1,9 @@
 package com.grupo.prisma;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
 
 
 @SpringBootApplication @ComponentScan(basePackages = { "com.*"})
